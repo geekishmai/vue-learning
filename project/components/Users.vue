5855b4d2429d2b25e479439a8f26906b8b36399f
mai <geekishmai@163.com>
2019-06-12 16:28:03 +0800

    “第一次提交”

---
+++ project/components/Users.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="hello">
+    <ul>
+      <li v-for="user in users" v-bind:key="user">{{user}}</li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HelloWorld',
+  props:{
+    users:{           //这个就是父组件中子标签自定义名字
+      type:Array,
+      required:true
+    }
+  }
+}
+</script>
---
+++ project/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <users v-bind:users="users"></users>
+  </div>
+</template>
+<script>
+import Users from "../components/Users"
+export default {
+  name: 'app',
+  data(){
+    return{
+      users:["Henry","Bucky","Emily"]
+    }
+  },
+  components:{
+    "users":Users
+  }
+}
+
+
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
